34e8ff99c36ab0bd438c8338276b033c4a8d44d6
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-09 23:49:27 +0100

    CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react"
 
+import Lightning from "./icons/lightning.png" 
+import Dots from "./icons/dots.png" 
+
 export default function App() {
     return (
-        <h1>Hello world</h1>
+        <main>
+            <h1 className="heading">Generate a<br /><span className="highlighted">random password</span></h1>
+            <h2 className="subtitle">Never use an insecure password again</h2>
+            <button className="generate-btn"><img src={Lightning} />Generate passwords</button>
+            <hr />
+            <div className="passwords">
+                <p className="password"><img src={Dots} /></p>
+                <p className="password"><img src={Dots} /></p>
+                <p className="password"><img src={Dots} /></p>
+                <p className="password"><img src={Dots} /></p>
+            </div>
+        </main>
     )
 }
