c7945a2d39936219cfaa0a5235935f3f94e6e304
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-10 12:00:56 +0100

    Updated styles.css

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import Dots from "./icons/dots.png"
 export default function App() {
     return (
         <main>
-            <h1 className="heading">Generate a<br /><span className="highlighted">random password</span></h1>
-            <h2 className="subtitle">Never use an insecure password again</h2>
-            <button className="generate-btn"><img src={Lightning} />Generate passwords</button>
+            <div className="header">
+                <h1 className="heading">Generate a<br /><span className="highlighted">random password</span></h1>
+                <h2 className="subtitle">Never use an insecure password again</h2>
+                <button className="generate-btn"><img src={Lightning} />Generate passwords</button>
+            </div>
             <hr />
-            <div className="passwords">
-                <p className="password"><img src={Dots} /></p>
-                <p className="password"><img src={Dots} /></p>
-                <p className="password"><img src={Dots} /></p>
-                <p className="password"><img src={Dots} /></p>
+            <div className="password-wrapper">
+                <div className="passwords">
+                    <p className="password"><img src={Dots} /></p>
+                    <p className="password"><img src={Dots} /></p>
+                    <p className="password"><img src={Dots} /></p>
+                    <p className="password"><img src={Dots} /></p>
+                </div>
             </div>
         </main>
     )
