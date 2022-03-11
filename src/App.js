089f7e2bbc701fd9056f29575a2ec7866a42c1e3
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-11 10:35:16 +0100

    Finished copying

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react"
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import Lightning from "./icons/lightning.png" 
 import Dots from "./icons/dots.png" 
 
 export default function App() {
     const [password, setPassword] = useState("")
-
+    const [copyed1, setCopyed1] = useState(false)
+    const [copyed2, setCopyed2] = useState(false)
+    const [copyed3, setCopyed3] = useState(false)
+    const [copyed4, setCopyed4] = useState(false)
+    
     function generatePassword() {
         var results = []
         for ( let j = 0; j < 4; j++ ) {
@@ -18,7 +23,34 @@ export default function App() {
             results[j] = result
         }
         setPassword(results)
-        console.log(password)
+    }
+
+    function copy1() {
+        setCopyed1(true)
+        setTimeout(() => {
+            setCopyed1(false)
+        }, 3000)
+    }
+
+    function copy2() {
+        setCopyed2(true)
+        setTimeout(() => {
+            setCopyed2(false)
+        }, 3000)
+    }
+
+    function copy3() {
+        setCopyed3(true)
+        setTimeout(() => {
+            setCopyed3(false)
+        }, 3000)
+    }
+
+    function copy4() {
+        setCopyed4(true)
+        setTimeout(() => {
+            setCopyed4(false)
+        }, 3000)
     }
 
     return (
@@ -31,10 +63,26 @@ export default function App() {
             <hr />
             <div className="password-wrapper">
                 <div className="passwords">
-                    <p className="password">{password ? <span>{password[0]}</span> : <img src={Dots} />}</p>
-                    <p className="password"><img src={Dots} />{password[1]}</p>
-                    <p className="password"><img src={Dots} />{password[2]}</p>
-                    <p className="password"><img src={Dots} />{password[3]}</p>
+                    <p className="password-display">
+                        <CopyToClipboard text={password[0]} onCopy={() => copy1()}>
+                            {copyed1 ? <span className="password">Copyed!</span> : password ? <span className="password">{password[0]}</span> : <img src={Dots} />}
+                        </CopyToClipboard>
+                    </p>
+                    <p className="password-display">
+                        <CopyToClipboard text={password[1]} onCopy={() => copy2()}>
+                            {copyed2 ? <span className="password">Copyed!</span> : password ? <span className="password">{password[1]}</span> : <img src={Dots} />}
+                        </CopyToClipboard>
+                    </p>
+                    <p className="password-display">
+                        <CopyToClipboard text={password[2]} onCopy={() => copy3()}>
+                            {copyed3 ? <span className="password">Copyed!</span> : password ? <span className="password">{password[2]}</span> : <img src={Dots} />}
+                        </CopyToClipboard>
+                    </p>
+                    <p className="password-display">
+                        <CopyToClipboard text={password[3]} onCopy={() => copy4()}>
+                            {copyed4 ? <span className="password">Copyed!</span> : password ? <span className="password">{password[3]}</span> : <img src={Dots} />}
+                        </CopyToClipboard>
+                    </p>
                 </div>
             </div>
         </main>
